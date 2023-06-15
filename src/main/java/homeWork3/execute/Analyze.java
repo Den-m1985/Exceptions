99fc63e01a3549b6b102456938e1dcca2eb5eff2
den-m1985 <denismartyanov244@gmail.com>
2023-06-15 14:25:26 +0200

    added class only letter

--- src/main/java/homeWork3/execute/Analyze.java
+++ src/main/java/homeWork3/execute/Analyze.java
@@ -18,18 +18,22 @@ public class Analyze {
 
         String[] divideStr = str.split(" ");
         PhoneNumber phoneNumber = new PhoneNumber();
+        OnlyLetters onlyLetters = new OnlyLetters();
 
         for (String string : divideStr) {
+            // проверем пол человека
             if (string.length() == 1) {
                 String temp = string.toUpperCase();
                 if (temp.equals("M") || temp.equals("F") || temp.equals("М") || temp.equals("Ж")) {
                     sex = temp;
                 } else throw new RuntimeException("Неверно написан пол человека");
 
+                // проверяем телефонный номер
             } else if (phoneNumber.isPhoneNumber(string)) {
                 phone = phoneNumber.strToNumber(string);
 
-            } else if (isOnlyLetters(string) && string.length() > 1) {
+                // проверяем ФИО
+            } else if (onlyLetters.isOnlyLetters(string) && string.length() > 1) {
                 if (lastName == null) {
                     lastName = capitalizeFirstLetter(string);
                 } else if (name == null) {
@@ -38,14 +42,15 @@ public class Analyze {
                     patronymic = capitalizeFirstLetter(string);
                 }
 
+                // проверяем дату рождения
             } else if (string.matches("\\d{2}\\.\\d{2}\\.\\d{4}")) {
                 ValidBirthDate validBirthDate = new ValidBirthDate();
                 try {
                     dateFormat = validBirthDate.getValidBirthDate(string);
                 } catch (ParseException e) {
-                    throw new RuntimeException("Неверный формат даты рождения");
+                    e.printStackTrace();
                 }
-            }
+            } else throw new RuntimeException("Не смог распознать строку: " + string);
         }
 
         String[] person = new String[6];
@@ -60,21 +65,9 @@ public class Analyze {
     }
 
 
-    public static boolean isOnlyLetters(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (!Character.isLetter(c)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
     public static String capitalizeFirstLetter(String str) {
-        if (str == null || str.isEmpty()) {
+        if (str == null || str.isEmpty())
             return str;
-        }
         return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
 
